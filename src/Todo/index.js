3785dd3139f0707b07eaaf8398da981dff6a3ab3
Hetvi Kanani <hetvipatel3110@gmail.com>
2021-04-07 18:58:20 +0530

    changes

--- src/Todo/index.js
+++ src/Todo/index.js
@@ -1,6 +1,6 @@
 import reactDom from "react-dom";
 import React, { Component } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import {
   Alert,
   InputGroup,
@@ -9,58 +9,97 @@ import {
   Input,
   Button,
   Table,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
 } from "reactstrap";
+
 
 import { Todostyle } from "./style";
 class Todo extends Component {
-
   state = {
     name: "",
     data: [],
     isUpdate: false,
-    updataId: ''
-  }
+    updataId: "",
+    isDeleteModal: false,
+    deleteId: "",
+  };
   addData = () => {
+    const xyz = this.state.data.filter((y) => y.name === this.state.name);
+    if (xyz.length > 0) {
+      alert(`${xyz[0].name} is already exit please enter differet todo name`);
+      return;
+    }
     if (this.state.name) {
       let todo = {};
-      todo.name = this.state.name
-      todo.id = uuidv4()
-      this.setState({ data: [...this.state.data, todo], name: "" })
+      todo.name = this.state.name;
+      todo.id = uuidv4();
+      this.setState({ data: [...this.state.data, todo], name: "" });
     }
-  }
+  };
+  resetData = () => {
+    this.setState({ name: "" });
+  };
   deleteData = (id) => {
-    let data = [...this.state.data]
-    const b = data.filter((h) => h.id !== id)
-    this.setState({ data: b })
-  }
+    let data = [...this.state.data];
+    const b = data.filter((h) => h.id !== this.state.deleteId);
+    this.setState({ data: b });
+    this.closeDeleteModal();
+  };
   updataData = (id) => {
-    let data = [...this.state.data]
-    const b = data.filter((h) => h.id === id)
-    this.setState({ name: b[0].name, isUpdate: true, updataId: id })
-  }
+    let data = [...this.state.data];
+    const b = data.filter((h) => h.id === id);
+    this.setState({ name: b[0].name, isUpdate: true, updataId: id });
+  };
   originalUpdate = () => {
-    console.log(this.state.updataId)
-    let data = [...this.state.data]
+    console.log(this.state.updataId);
+    let data = [...this.state.data];
     const b = data.map((h) => {
       if (h.id === this.state.updataId) {
-        h.name = this.state.name
-        
+        h.name = this.state.name;
       }
       return h;
-    })
-    this.setState({ data: b, isUpdate: false, name: "", updataId: '' })
+    });
+    this.setState({ data: b, isUpdate: false, name: "", updataId: "" });
+  };
+  closeDeleteModal = () => {
+    this.setState({ isDeleteModal: false, deleteId: "" })
+  }
+  openDeleteModal = (id) => {
+    this.setState({ isDeleteModal: true, deleteId: id })
+
+
   }
   render() {
+    const { isDeleteModal } = this.state
     return (
       <Todostyle>
-        {console.log(this.state, "aaaaa")}
         <h2>My To Do List</h2>
         <div className="box">
           <div className="input">
             <InputGroup>
-              <Input value={this.state.name} placeholder="Enter Text" onChange={(e) => { this.setState({ name: e.target.value }) }} />
+              <Input
+                value={this.state.name}
+                placeholder="Enter Text"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+              />
             </InputGroup>
-            {!this.state.isUpdate ? <Button color="secondary" onClick={this.addData}>Add</Button> : <Button color="secondary" onClick={this.originalUpdate}>update</Button>}
+            {!this.state.isUpdate ? (
+              <Button color="secondary" onClick={this.addData}>
+                Add
+              </Button>
+            ) : (
+              <Button color="secondary" onClick={this.originalUpdate}>
+                Update
+              </Button>
+            )}
+            <Button color="secondary" onClick={this.resetData}>
+              Reset
+            </Button>
           </div>
         </div>
         <Table dark>
@@ -72,21 +111,49 @@ class Todo extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.data.map((x, i) => {
-              return <>
-                <tr>
-                  <th scope="row">{i + 1}</th>
-                  <td>{x.name}</td>
-                  <td><Button color="primary" onClick={() => this.deleteData(x.id)}>delete</Button>
-                    <Button color="secondary" onClick={() => this.updataData(x.id)}>edit</Button></td>
-                </tr>
-              </>
+            {this.state.data.map((x, i) =>
+            (
 
-            })}
+              <tr key={x.id}>
+                <th scope="row">{i + 1}</th>
+                <td>{x.name}</td>
+                <td>
+                  <Button
+                    color="danger"
+                    onClick={() => this.openDeleteModal(x.id)}
+                  >
+                    Delete
+                      </Button>
+                  <Button
+                    color="primary"
+                    style={{ marginLeft: "10px" }}
 
+                    onClick={() => this.updataData(x.id)}
+                  >
+                    Edit
+                      </Button>
+                </td>
+              </tr>
+
+            )
+            )}
           </tbody>
-
         </Table>
+
+        <Modal isOpen={isDeleteModal} toggle={this.closeDeleteModal} >
+
+          <ModalHeader toggle={this.closeDeleteModal}>Modal title</ModalHeader>
+          <ModalBody>
+            Are you sure you want to delete this?
+            </ModalBody>
+
+          <ModalFooter>
+            <Button color="danger" onClick={this.deleteData}>Delete</Button>
+            <Button color="secondary" onClick={this.closeDeleteModal}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+
+
       </Todostyle>
     );
   }
